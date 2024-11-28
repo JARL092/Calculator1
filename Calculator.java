88016350a7b64be05c2ac81b894c23d3c7e7edc0
JARL092 <106961602+JARL092@users.noreply.github.com>
2024-11-28 12:48:24 +0200

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,9 @@
-// src/com/example/Calculator.java
-package com.example;
-
 public class Calculator {
     public static int add(int a, int b) {
         return a + b;
     }
 
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 20;
-        System.out.println("Сума " + num1 + " і " + num2 + " дорівнює: " + add(num1, num2));
+        System.out.println("Sum: " + add(3, 5));
     }
 }
