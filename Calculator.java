90b2b9a30a6450feb84c5d69fcea9d76acf91ec6
JARL092 <106961602+JARL092@users.noreply.github.com>
2024-11-28 14:30:53 +0200

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,15 @@ public class Calculator {
         return a + b;
     }
 
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Sum: " + add(3, 5));
+        int result1 = add(5, 3);
+        int result2 = subtract(10, 4);
+
+        System.out.println("Addition result: " + result1);
+        System.out.println("Subtraction result: " + result2);
     }
 }
